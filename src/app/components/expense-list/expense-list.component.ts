122308683bc0c3d98e55f7a219d54a6ad004bd5c
VelosoN97 <nicolas.veloso0397@gmail.com>
2025-03-12 13:14:21 -0300

    Commit 5 - Creación Vista Expense List

---
+++ src/app/components/expense-list/expense-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Expense } from '../../models/expense.model';
+import { ExpenseService } from '../../services/expense.service';
+
+@Component({
+  selector: 'app-expense-list',
+  templateUrl: './expense-list.component.html',
+  styleUrl: './expense-list.component.css'
+})
+export class ExpenseListComponent implements OnInit {
+  expenses: Expense[] = [];
+  total: number = 0;
+
+  constructor(
+    private expenseService: ExpenseService
+  ){}
+
+  ngOnInit(): void {
+      this.loadDataIntoTable();
+  }
+
+  private loadDataIntoTable(): void {
+    this.expenseService.getExpenses().subscribe(expenses => {
+      this.expenses = expenses;
+      this.calculeTotal();
+    });
+  }
+
+  private calculeTotal(): void{
+    this.total = this.expenses.reduce((accumulated, currentValue) => {
+      return accumulated + Number(currentValue.amount);
+    }, 0);
+  }
+}
